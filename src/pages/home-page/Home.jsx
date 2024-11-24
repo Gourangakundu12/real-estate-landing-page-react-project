e6e8e03904b26c9f72eeaf4868f1d356600c05ae
ishanrana363 <ishanrana363@gmail.com>
2024-11-24 23:44:10 +0600

    service added

--- src/pages/home-page/Home.jsx
+++ src/pages/home-page/Home.jsx
@@ -4,11 +4,13 @@ import HeroSection from './HeroSection'
 import Update from './Update'
 import AboutUs from './AboutUs'
 import UniqueHome from './UniqueHome'
+import EstateService from './EstateService';
 
 const Home = () => {
   return (
     <div>
       {/* <Banner></Banner> */}
+      <EstateService></EstateService>
       <UniqueHome></UniqueHome>
       <AboutUs></AboutUs>
       <Update></Update>
